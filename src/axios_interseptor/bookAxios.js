ef4b871eb666cfa7068d139a930259619057e8eb
anasask2001 <anas.p8113@gmail.com>
2024-11-14 11:23:40 +0530

    "first commit"

--- src/axios_interseptor/bookAxios.js
+++ src/axios_interseptor/bookAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const bookInstance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:3002/api" 
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL || "https://ibst-book-app-back-end.onrender.com/api" 
 });
 
 export default bookInstance;
